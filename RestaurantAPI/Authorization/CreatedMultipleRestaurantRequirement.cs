6ca156003711eb3d6b1e46daf4656c8ced4e97c6
Filip Statkiewicz <stf6.dev@gmail.com>
2024-06-03 21:44:12 +0200

    Add project files.

---
+++ RestaurantAPI/Authorization/CreatedMultipleRestaurantRequirement.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace RestaurantAPI.Authorization
+{
+    public class CreatedMultipleRestaurantRequirement : IAuthorizationRequirement
+    {
+
+        public CreatedMultipleRestaurantRequirement(int minimumRestaurantCreated)
+        {
+            MinimumRestaurantCreated = minimumRestaurantCreated;
+        }
+
+        public int MinimumRestaurantCreated { get; }
+    }
+}
